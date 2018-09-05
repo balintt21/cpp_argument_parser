551811dc469a5577994a16527ec3325865ad03fd
balintt21 <balintt21@gmail.com>
2018-09-05 07:17:39 +0200

    Added header only argument parser

---
+++ argument_parser.h
@@ -0,0 +1,73 @@
+#ifndef _ARGUMENT_PARSER_H_
+#define _ARGUMENT_PARSER_H_
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+
+namespace argument_parser
+{
+    std::vector<std::string> read(int argc, char** argv)
+    {
+        std::vector<std::string> arguments;
+        arguments.reserve(argc);
+        for(int i = 0; i < argc; ++i){ arguments.emplace_back(argv[i]); }
+        return std::move(arguments);
+    }
+
+    std::vector<std::string>::const_iterator find(const std::string& short_opt, const std::string& long_opt, const std::vector<std::string>& args)
+    {
+        return std::find_if(args.cbegin(), args.cend(), [long_opt, short_opt](const std::string& option) -> bool 
+        {
+            return ( (option.find(long_opt) != std::string::npos ) || (!short_opt.empty() && ((option[0] == '-') && (option[1] != '-') && (option.find(short_opt[1]) != std::string::npos))) );
+        });    
+    }
+
+    bool has(const std::string& short_opt, const std::string& long_opt, const std::vector<std::string>& args)
+    { 
+        return find(short_opt, long_opt, args) != args.cend(); 
+    }
+
+    std::string get(std::vector<std::string>::const_iterator& arg_it, const std::vector<std::string>& args)
+    {
+        if( arg_it != args.cend() )
+        {
+            const std::string& option = (*arg_it);
+            size_t pos = 0;
+            if( (pos = option.find('=')) != std::string::npos )
+            {
+                return option.substr(pos+1);
+            } else {
+                ++arg_it;
+                if( arg_it != args.cend() )
+                {
+                    return (*arg_it);
+                }
+            }
+        }
+        return "";
+    }
+
+    int64_t getInt(std::vector<std::string>::const_iterator& arg_it, const std::vector<std::string>& args)
+    {
+        std::string value = get(arg_it, args);
+        if( !value.empty() )
+        {
+            return std::strtoll(value.c_str(), nullptr, 10);
+        }
+        return 0;
+    }
+
+    int64_t getDouble(std::vector<std::string>::const_iterator& arg_it, const std::vector<std::string>& args)
+    {
+        std::string value = get(arg_it, args);
+        if( !value.empty() )
+        {
+            return std::strtold(value.c_str(), nullptr);
+        }
+        return 0.0f;
+    }
+}
+
+#endif
